b6a8ec6437f5e9233fc898c38cd857ea8c59000b
Brand0nKelly <c19428962@mytudublin.ie>
2021-05-06 22:45:14 +0100

    Improved Waveform

--- java/src/C19428962/WaveForm.java
+++ java/src/C19428962/WaveForm.java
@@ -6,14 +6,20 @@ import processing.core.*;
 public class WaveForm
 {
     MyVisual bk; 
+
     float cy = 0;
+
+    float yc = 0;
 
     
 
     public WaveForm(MyVisual bk)
     {
         this.bk = bk;
-        cy = this.bk.height;
+
+        cy = this.bk.height / 4;
+
+        cy = this.bk.width / 4;
 
     
     }
@@ -29,17 +35,10 @@ public class WaveForm
                 , 255
             );
             bk.line(i, cy, i, cy + cy * bk.getAudioBuffer().get(i));
+
+            bk.line(i, cy *2, i, cy *2 + cy * bk.getAudioBuffer().get(i));
         }
 
-        for(int i = 0 ; i < bk.getAudioBuffer().size() ; i ++)
-        {
-            bk.stroke(
-                PApplet.map(i, 0, bk.getAudioBuffer().size(), 0, 225)
-                , 255
-                , 255
-            );
-            bk.line(i, 10, i, 10 + 10 * bk.getAudioBuffer().get(i));
-        }
 
 
      
